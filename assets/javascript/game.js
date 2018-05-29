c7756c142d5acc17b732bd5300da2995dc1c867a
Francisco <belcboo@gmail.com>
2018-05-28 22:05:07 -0400

    Deleted the console logs from the js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,19 @@ var wins = 0; /* Stores wins of the user */
 var losses = 0; /* Store loses of user/wins of the system */
 var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; /* Store the vales that the system will pick random */
 var guess = 9; /* Counter set to 9, each run will substrac 1 */
-var chain = ""; /* Chain to store the letters typed by user */
+var chain = ""; /* Chain to store the letters typed by user *
 
 /* This function will be triggered each time the user press a key */
 document.onkeyup = function (event) {
 
     var userInput = event.key.toLowerCase(); /* Assign the key entered by user as value and convert it to lowercase */
     var pyschic = abc[Math.floor(Math.random() * abc.length)]; /* Assign a random value to psychic */
-
+ 
     guess = guess - 1; /* Decreases the counter to validate winner */
     document.getElementById("guesses").innerHTML = "Guesses Left: " + guess;
     chain = chain + event.key + ", "; /* Add's letters entered by user to create a chain and display user guesses */
     document.getElementById("chain").innerHTML = "Your guesses so far: " + chain; /* Display on website the user guesses */
+    
 
     if (pyschic === userInput) { /* If user guesses the system random letter then: */
         wins++; /* Adds one to var wins */
